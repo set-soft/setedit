15a793cf9c12e99a787b627bff340354deee198c
set <>
2001-10-01 17:11:49 +0000

    * Modified: Moved all the macros to define if we need \r\n or \n from setedit to the compatlayer library.

--- setedit/setedit/streams/sdskhelp.cc
+++ setedit/setedit/streams/sdskhelp.cc
@@ -1,10 +1,11 @@
-/* Copyright (C) 1996,1997,1998,1999,2000 by Salvador E. Tropea (SET),
+/* Copyright (C) 1996-2001 by Salvador E. Tropea (SET),
    see copyrigh file for details */
 #define Uses_TCEditWindow
 #define Uses_TWindow
 
 #define Uses_TStreamableClass
 #define Uses_TStreamable
+#define Uses_string
 #include <ceditor.h>
 #include <dskwin.h>
 #include <dskhelp.h>
