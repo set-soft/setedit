ad633523606148ad4d61f7dbc254ac29155febab
set <>
2001-10-01 16:31:04 +0000

    * Modified: As now compatlayer defines uint* I needed to fix some collisions.

--- setedit/names/neditwin.cc
+++ setedit/names/neditwin.cc
@@ -1,6 +1,5 @@
-/* Copyright (C) 1996,1997,1998,1999,2000 by Salvador E. Tropea (SET),
+/* Copyright (C) 1996-2001 by Salvador E. Tropea (SET),
    see copyrigh file for details */
-#include <tv/ttypes.h>
 #define Uses_n
 #include <tv/tvutil.h>
 n(TCEditWindow);
