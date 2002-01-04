f2c4ffe937bf1626dcd3560cdfc5a75ce549e694
Salvador E. Tropea <salvador@inti.gob.ar>
2002-01-04 12:46:56 +0000

    * Modified: Moved rhtv and z libraries to .env/configuration. * Fixed: pcre library was mandatory. * Added: MSS 1.2.1 support --with-mss configuration option.

--- setedit/configset.bat
+++ setedit/configset.bat
@@ -3,6 +3,11 @@
 @
 @Echo off
 @del configure.cache
+@del Makefile
 @set CFLAGS=-O2 -Wall -Werror -gstabs+3
 @set CXXFLAGS=-O2 -Wall -Werror -gstabs+3 -fno-exceptions -fno-rtti
-@configure.bat "--Xcppflags=-O3 -fomit-frame-pointer -ffast-math -fno-exceptions -fno-rtti" --shipped-intl --no-prefix-h
+@echo "--Xcppflags=-O3 -fomit-frame-pointer -ffast-math -fno-exceptions -fno-rtti" --shipped-intl > perl.parm
+@echo --no-prefix-h %1 %2 %3 %4 %5 %6 %7 %8 %9 >> perl.parm
+@perl config.pl @perl.parm
+@del perl.parm
+
