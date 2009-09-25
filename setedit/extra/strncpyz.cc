89eb18e3b4ed3ea03c3c4b045bc403436cd447ef
Salvador E. Tropea <salvador@inti.gob.ar>
2009-09-25 15:08:54 +0000

    * Modified: To support passing a NULL pointer for orig.

--- setedit/extra/strncpyz.cc
+++ setedit/extra/strncpyz.cc
@@ -1,10 +1,10 @@
-/* Copyright (C) 1996-2003 by Salvador E. Tropea (SET),
+/* Copyright (C) 1996-2009 by Salvador E. Tropea (SET),
    see copyrigh file for details */
 char *strncpyZ(char *dest, const char *orig, int size)
 {
  if (size)
     size--;
- for (;*orig && size; size--,dest++,orig++)
+ for (;orig && *orig && size; size--,dest++,orig++)
      *dest=*orig;
  *dest=0;
  return dest;
